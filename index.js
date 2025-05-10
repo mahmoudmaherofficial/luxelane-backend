764b200f8a74d9c02fbec4e68322400366554cd5
Mahmoud Maher <mahmoudmaherofficial@gmail.com>
2025-05-10 05:46:39 +0300

    added: client_url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser()); // Must be before routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
